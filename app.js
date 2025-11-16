ba8014306b37d00ca4d82c1675c7d22f9b06ec67
GustavoAngel <gustavoandradeangel@hotmail.com>
2025-11-15 23:07:00 -0600

    x1
    x2

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
  // --- Funciones del Modal ---
-        function showCustomModal(title, message) {
+        function showCustomModal(title, message, type = 'alert',callback = null) {
             document.getElementById('modal-title').textContent = title;
             document.getElementById('modal-message').textContent = message;
             document.getElementById('custom-modal-overlay').classList.add('visible');
+            if (type === 'confirm' && callback) {
+                const confirmButton = document.querySelector('.modal-confirm-button');
+                confirmButton.onclick = function() {
+                    hideCustomModal();
+                    callback(true);
+                };
+            } else if (type === 'alert' && callback) {
+                const confirmButton = document.querySelector('.modal-confirm-button');
+                confirmButton.onclick = function() {
+                    hideCustomModal();
+                    callback();
+                };
+            }
+            
         }
 
         function hideCustomModal() {
